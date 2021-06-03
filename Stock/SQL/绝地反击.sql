a1eca07deae0daeb8a454e9985b4e0d872e5a2ff
2984294509@qq.com <2984294509@qq.com>
2021-06-04 02:07:19 +0800

    1

---
+++ Stock/SQL/绝地反击.sql
@@ -0,0 +1,102 @@
+--SCS买点2：以退为进
+--买点描述：股价在连续下跌后收出低位大阴线形态，第二天股价低开后高走，盘间最高价回补缺口
+---最终低开收阳的倒锤子形态
+ 
+ -----------------------------------------------------------------------------------
+WITH    T AS ( SELECT   ( CASE WHEN ( shou - kai ) > 0 THEN 1
+                               WHEN ( shou - kai ) = 0 THEN 0
+                               WHEN ( shou - kai ) < 0 THEN -1
+                          END ) AS zhangdie ,
+                        ( shou - kai ) AS shiti ,
+                        ( shou - kai ) / kai * 100 AS shitifudu ,
+                        [code] ,
+                        [riqi] ,
+                        [kai] ,
+                        [shou] ,
+                        [di] ,
+                        [gao] ,
+                        [chengjiaoliang] ,
+                        1 AS [pctChg]
+               FROM     dbo.lishijiager
+               WHERE    riqi >= DATEADD(DAY, -21, GETDATE())
+             )-----------------------------------------------------------------
+ ,      T2
+          AS ( SELECT   ( CASE zhangdie
+                            WHEN 1 THEN ( gao - shou )
+                            WHEN -1 THEN ( kai - gao )
+                          END ) AS shanyingxian ,
+                        ( CASE zhangdie
+                            WHEN 1 THEN ( kai - di )
+                            WHEN -1 THEN ( di - shou )
+                          END ) AS xiayingxian ,
+                        *
+               FROM     T
+             )----------------------------------------------------------------
+	,   T3
+          AS ( SELECT   shanyingxian / shiti AS syxbst ,
+                        xiayingxian / shiti AS xyxbst ,
+                        ROW_NUMBER() OVER ( PARTITION BY code ORDER BY gao DESC ) AS RowID ,
+						
+                        *
+               FROM     T2
+             ),
+        T4
+          AS ( SELECT   *
+               FROM     T3
+               WHERE    RowID = 1
+             )-----------------------------------------------------------------------
+	,   T5
+          AS ( SELECT   COUNT(1) OVER ( PARTITION BY T3.code ) AS zhangdiezhouqishu ,
+                        T4.code ,
+                        T4.riqi AS kaishiriqi ,
+                        T3.riqi ,
+                        T3.shiti ,
+                        T3.shitifudu ,
+                        T3.zhangdie ,
+                        T3.syxbst ,
+                        T3.xyxbst,
+						T3.di,
+						T3.kai,
+						T3.shou,
+						T3.gao,
+						MIN(T3.shitifudu) OVER(PARTITION BY T3.code) AS zuidadiefu
+               FROM     T4
+                        INNER JOIN T3 ON T4.code = T3.code
+                                         AND T4.riqi < T3.riqi
+               WHERE    T4.RowID = 1
+             ),
+        T6
+          AS ( SELECT   ROW_NUMBER() OVER ( PARTITION BY code ORDER BY riqi ) AS riqihao ,
+		   ROW_NUMBER() OVER ( PARTITION BY code ORDER BY di DESC ) AS zuidijiahao ,
+                        *
+               FROM     T5
+             ),
+        T7
+          AS ( SELECT   riqihao
+                        - ROW_NUMBER() OVER ( PARTITION BY code ORDER BY riqi ) AS lianxuxiadieriqizu ,
+                        *
+               FROM     T6
+               WHERE   --- code='sh.603985' AND   
+                        zhangdie = -1
+             ),
+        T8
+          AS ( SELECT   COUNT(1) OVER ( PARTITION BY code, lianxuxiadieriqizu ) AS lianxuxiadieshu ,
+                        *
+               FROM     T7
+             ),
+        T9
+          AS ( SELECT   MAX(lianxuxiadieshu) OVER ( PARTITION BY code ) zuidalianxuxiadieshu ,
+		   MAX(riqihao) OVER ( PARTITION BY code ) zuidariqihao ,
+                        *
+               FROM     T8
+             )
+			 ,T10 AS (
+			 SELECT * FROM T9 WHERE riqihao=zhangdiezhouqishu-1 AND  T9.shitifudu=zuidadiefu
+			 		 			 AND zuidijiahao=zhangdiezhouqishu
+			 )
+			
+		 SELECT * FROM T10 INNER JOIN T6 ON  T10.code = T6.code
+		 WHERE  T10.riqihao+1=t6.riqihao AND T10.shou>T6.kai AND T6.gao>T10.shou  AND  t6.shitifudu<8
+		 AND T6.syxbst>1 AND  t6.di/t6.kai<=1
+		 		
+		  
