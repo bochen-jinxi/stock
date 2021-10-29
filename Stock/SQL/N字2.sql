0f1db26eafbfb13225f552984902948e6a6ee044
bochen-jinxi <60917379+bochen-jinxi@users.noreply.github.com>
2021-10-29 16:58:36 +0800

    1
    2

--- Stock/SQL/N字2.sql
+++ Stock/SQL/N字2.sql
@@ -6,9 +6,8 @@
 WITH    T AS ( SELECT   ( CASE WHEN ( shou - kai ) > 0 THEN 1
                                WHEN ( shou - kai ) = 0 THEN 0
                                WHEN ( shou - kai ) < 0 THEN -1
-                          END ) AS zhangdie ,
-                        ( shou - kai ) AS shiti ,
-                        ( shou - kai ) / kai * 100 AS shitifudu ,
+                          END ) AS zhangdie ,                  
+                   
                         [code] ,
                         [riqi] ,
                         [kai] ,
@@ -25,11 +24,18 @@ WITH    T AS ( SELECT   ( CASE WHEN ( shou - kai ) > 0 THEN 1
 	,   T2
           AS (
 		  --取最大值时间  最小值时间
-		   SELECT  
-		     ROW_NUMBER() OVER ( PARTITION BY code ORDER BY riqi asc   ) AS riqihao2 ,
-		     ROW_NUMBER() OVER ( PARTITION BY code ORDER BY riqi DESC  ) AS riqihao ,
-		   ROW_NUMBER() OVER ( PARTITION BY code ORDER BY gao ASC ) AS zuidijiahao ,
+		   SELECT     ( CASE zhangdie WHEN 1 Then ( shou - kai ) / kai END) * 100 AS shitifudu ,
+						      (( CASE zhangdie
+                            WHEN 1 THEN ( gao - shou )
+                          END )/ ( CASE zhangdie WHEN 1 Then  ( shou - kai )
+						 END)) AS syxbst ,
+						    ( CASE zhangdie
+                            WHEN 1 THEN ( gao - shou )/shou
+                          END )*100 AS syxfd ,
 
+		ROW_NUMBER() OVER ( PARTITION BY code ORDER BY riqi asc   ) AS riqihao2 ,
+		     ROW_NUMBER() OVER ( PARTITION BY code ORDER BY riqi DESC  ) AS riqihao ,
+	
                         ROW_NUMBER() OVER ( PARTITION BY code ORDER BY gao DESC ) AS RowID ,
 						ROW_NUMBER() OVER ( PARTITION BY code ORDER BY di Asc ) AS RowID2 ,
                         *
@@ -41,7 +47,7 @@ WITH    T AS ( SELECT   ( CASE WHEN ( shou - kai ) > 0 THEN 1
 	   )
 
 	    ,T4 AS (SELECT T2.* FROM T2   WHERE RowID2=1      ) 
-			--	 select  * from T4 order by riqi
+			--	 见高点后下跌4天之内见地点
 		,T5 AS  ( 
 			 SELECT T2.riqihao2-T3.riqihao2 as num  , T3.riqi AS griqi,T2.riqi AS d1riqi,T2.*,T2.di as djiage FROM  T3   INNER JOIN  T2 ON T3.code = T2.code  
 			WHERE   (T2.riqihao2-1=T3.riqihao2 or T2.riqihao2-2=T3.riqihao2 or T2.riqihao2-3=T3.riqihao2 or T2.riqihao2-4=T3.riqihao2 )   and T2.zhangdie=-1
@@ -53,14 +59,28 @@ WITH    T AS ( SELECT   ( CASE WHEN ( shou - kai ) > 0 THEN 1
 		 select ROW_NUMBER() over(PARTITION BY code order by riqihao2 desc) as num3, * from T6  )
 		 ,T8 as (
 		 select * from T7 where num3=1)
-		 select T8.djia,T4.di, T8.riqihao2,T4.riqihao2, jin4tianxiadieshu,T4.code,griqi,d1riqi,T4.riqi as d2riqi from T8 inner join T4 on T4.code=T8.code
+	,T9 AS (
+		 select T8.djia,T4.di, T8.riqihao2 AS  minriqihao2,T4.riqihao2 AS maxriqihao2, jin4tianxiadieshu,T4.code,griqi,d1riqi,T4.riqi as d2riqi from T8 inner join T4 on T4.code=T8.code
 		 inner join T2 on T8.code=T2.code
 		  where T8.di>=T4.di
 	  and  T8.djia-T4.di>0
-		 and ((T8.djia-T4.di)/t8.djia)*100<4
+	  --1底点 和 2底点之间地点间距6各点之内
+		 --and ((T8.djia-T4.di)/t8.djia)*100<6
 		 and T4.riqi>d1riqi
 		 and T2.riqi='2021-10-28' 
-		 --and T2.zhangdie=1
-		 and T8.riqihao2<T4.riqihao2-2
-		 order by T4.shou desc 
+		--and T2.zhangdie=1
+		 and T8.riqihao2<T4.riqihao2-2)
+		 --上冲2个点以上 回落
+		 SELECT T2.shou,  T9.*, T2.riqi AS syxrq,   T2.riqihao2  FROM T9 INNER JOIN  T2  ON T2.code=T9.code WHERE     T2.riqihao2>=minriqihao2 AND T2.riqihao2<=maxriqihao2  
+	--AND syxbst>1
+	 AND syxfd>2
+	 ORDER BY 1 DESC 
+
+
+
+ 
+
+
+	
+		 
   
