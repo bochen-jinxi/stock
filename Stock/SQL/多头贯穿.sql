cecb94b08b03c5ae4cdd562e0b436af58ddc810c
chenbo <2984294503@qq.com>
2021-12-27 14:13:25 +0800

    1

--- Stock/SQL/多头贯穿.sql
+++ Stock/SQL/多头贯穿.sql
@@ -16,19 +16,20 @@ WITH    T AS ( SELECT
                          [pctChg] AS zf						
 						 
                FROM     dbo.lishijiager
- WHERE    riqi >='2021-12-20' AND  riqi<='2021-12-23'
+ WHERE    riqi >='2021-12-20' AND  riqi<='2021-12-24'
 							)
 
 							SELECT  * FROM T   INNER JOIN T AS T0   ON T.code = T0.code 
 							WHERE T.riqidaoxu=T0.riqidaoxu-1   
 							--前一天阴
 							AND (T0.di*1.03)>=T0.shou AND T0.zf<0
-							--AND T0.riqidaoxu=2
-							-- 后一天贯穿
-							AND T.kai*1.01<=T0.di   AND T.shou>=T0.kai/1.05 
-							AND T.zf>=0
-							AND T.riqi='2021-12-23'
+							-- 前一天跌幅最大
 							AND T0.zfzhengxu=1
+							
+							-- 后一天至少低开一个点 收贯穿
+							AND T.kai*1.01<=T0.di   AND T.shou>=(T0.shou-T0.kai)/(100/50)+T0.kai 
+							AND T.zf>=0
+							AND T.riqi='2021-12-24'
 							ORDER BY T0.zf 
 
 		 		
