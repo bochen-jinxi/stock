6a291068467ac4eaa2df661cd2920288ed3dbf72
bochen-jinxi <60917379+bochen-jinxi@users.noreply.github.com>
2022-04-10 15:08:36 +0800

    1
    1

--- Stock/SQL/定时炸弹- 副本.sql
+++ Stock/SQL/定时炸弹- 副本.sql
@@ -1,8 +1,8 @@
-﻿--SCS买点4：破低反涨
---买点描述：股价在近期支撑位出现大阴线破位走势，第二天大阳能强势收复。 肉眼可见前期有一波淋漓的上涨
+﻿--SCS买点5：定时炸弹
+--买点描述：股价在近期支撑位出现大阴线破位走势，第二天小大阳高开收阳，第三天收复前2天阴线 肉眼可见前期有一波淋漓的上涨
  
  -----------------------------------------------------------------------------------
-    --找最近8个交易日的K线
+    --找最近21个交易日的K线
 	  use stock 
    go 
 WITH    T AS ( SELECT   ( CASE WHEN ( shou - kai ) > 0 THEN 1
@@ -21,8 +21,8 @@ WITH    T AS ( SELECT   ( CASE WHEN ( shou - kai ) > 0 THEN 1
                         1 AS [pctChg]
                FROM     dbo.lishijiager
               -- WHERE    riqi >= DATEADD(DAY, -21, GETDATE())
-			  WHERE    riqi >='2022-03-01' AND  riqi<='2022-04-01'
-			   --AND   code LIKE '%000090%'
+			  WHERE    riqi >='2022-03-20' AND  riqi<='2022-04-08'
+			 --AND   code LIKE '%000422%'
              )-----------------------------------------------------------------
  ,      T2
           AS ( 
@@ -43,18 +43,22 @@ WITH    T AS ( SELECT   ( CASE WHEN ( shou - kai ) > 0 THEN 1
 		   --冲高回落/探底回升的比例 
 		  SELECT   shanyingxian / shiti AS syxbst ,
                         xiayingxian / shiti AS xyxbst ,
-                        ROW_NUMBER() OVER ( PARTITION BY code ORDER BY gao DESC ) AS RowID ,
+                        ROW_NUMBER() OVER ( PARTITION BY code ORDER BY gao DESC,riqi ASC  ) AS RowID ,
 						
                         *
                FROM     T2
-             ),
+             )
+			--SELECT * FROM T3
+			 ,
         T4
           AS ( 
 		    -- 各代码见高点的日期 价格
 		  SELECT   *
                FROM     T3
                WHERE    RowID = 1
-             )-----------------------------------------------------------------------
+             )
+			 
+			 -----------------------------------------------------------------------
 	,   T5
           AS (
 		  	--见高点后 后续价格数据中所有阴阳线 并统计后续阴阳线的数量
@@ -74,9 +78,11 @@ WITH    T AS ( SELECT   ( CASE WHEN ( shou - kai ) > 0 THEN 1
 						MIN(T3.shitifudu) OVER(PARTITION BY T3.code) AS zuidadiefu
                FROM     T4
                         INNER JOIN T3 ON T4.code = T3.code
-                                         AND T4.riqi < T3.riqi
-               WHERE    T4.RowID = 1
-             ),
+                         AND T4.riqi < T3.riqi   
+               WHERE    T4.RowID = 1  
+             )
+			-- SELECT * FROM T5
+			 ,
         T6
           AS ( 
 		      -- 后续数据按日期正序标号  最低价倒序
@@ -84,7 +90,9 @@ WITH    T AS ( SELECT   ( CASE WHEN ( shou - kai ) > 0 THEN 1
 		   ROW_NUMBER() OVER ( PARTITION BY code ORDER BY di DESC ) AS zuidijiahao ,
                         *
                FROM     T5
-             ),
+             )
+			 --SELECT * FROM T6
+			 ,
         T7
           AS (
 		   --查找后续中所有阴线并重新按日期正序标号 用以查找连续日期号的阴线
@@ -95,7 +103,9 @@ WITH    T AS ( SELECT   ( CASE WHEN ( shou - kai ) > 0 THEN 1
                FROM     T6
                WHERE   --- code='sh.603985' AND   
                         zhangdie = -1
-             ),
+             )
+			 
+			 ,
         T8
           AS (
 		   --标识后续中所有连续阴线的天数
@@ -110,23 +120,26 @@ WITH    T AS ( SELECT   ( CASE WHEN ( shou - kai ) > 0 THEN 1
                         *
                FROM     T8
              )
+			 
 			 ,T10 AS (
-			 --查找后续最大跌幅的阴线 并且是上一个交易日期
-			 SELECT * FROM T9 WHERE riqihao+1=zhangdiezhouqishu 
+			 --查找后续最大跌幅的阴线 并且是上二个交易日期
+			 SELECT * FROM T9 WHERE riqihao+2=zhangdiezhouqishu 
 			-- AND  T9.shitifudu=zuidadiefu
 			 		 			-- AND zuidijiahao=zhangdiezhouqishu
 			 )
 			 --SELECT * FROM T10
-			--第二天阳线收复
-		 SELECT DISTINCT T10.kaishiriqi,T10.code,T10.riqi,T6.riqi,  T10.shitifudu,T6.shitifudu,T10.yingxianshu,T10.yangxianshu, T10.* FROM T10 INNER JOIN T6 ON  T10.code = T6.code
-		 WHERE  
-	     T10.riqihao+1=t6.riqihao 
-		 AND T10.zhangdie=-1  AND  ABS(T10.shitifudu)/(100/61.8)<T6.shitifudu
-		  and T10.kai>=T6.shou	
-	     AND (T10.zuidijiahao=T10.zhangdiezhouqishu	 OR T10.zuidijiahao+1=T10.zhangdiezhouqishu)
-		 --AND T10.zhangdiezhouqishu>8	 	
-		 AND T10.yingxianshu<T10.yangxianshu
-			 	AND 		  T6.riqi='2022-04-01'	
-				 ORDER BY T10.code
+			--第二天小阳线高开手阳
+		 SELECT DISTINCT A.kaishiriqi,A.code,A.riqi,B.riqi,  A.shitifudu,B.shitifudu,A.yingxianshu,A.yangxianshu, A.*
+		
+		  FROM T10 AS A INNER JOIN T6 AS B ON  A.code = B.code
+		 INNER JOIN T6 AS C ON  B.code = C.code
+		 WHERE   A.riqihao+2=C.riqihao AND   B.riqihao+1=C.riqihao	 	
+		 AND A.zhangdie=-1   AND A.shou<=A.di*1.02 AND A.kai>=A.gao/1.02
+		AND B.zhangdie=1 
+		AND B.kai=B.di		
+		--AND B.kai>A.shou*1.00001	
+		AND C.shou>A.kai
+	   
+      ORDER BY A.code
 		  
  
