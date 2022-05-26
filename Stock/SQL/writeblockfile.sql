a54793cceb22eb7d11ae3ee24a142f38335e3043
bochen-jinxi <60917379+bochen-jinxi@users.noreply.github.com>
2022-05-26 20:48:57 +0800

    1
    1

--- Stock/SQL/writeblockfile.sql
+++ Stock/SQL/writeblockfile.sql
@@ -1,4 +1,4 @@
- --查询排序
+  --查询排序
  --SELECT * FROM  dbo.T900
  --WHERE zhuriqi='2022-04-13'
  --ORDER BY code desc
@@ -8,7 +8,8 @@
 SELECT  DISTINCT   CONCAT('exec master.dbo.xp_cmdshell ''echo '+CONVERT(varchar(5), zhuriqi, 10)+'                                             '+CONVERT(varchar(5), zhuriqi, 10)+'                                                               >>C:\zd_zsone\T0002\blocknew\blocknew.cfg"''',';')
   ,  zhuriqi,
    CONVERT(varchar(5), zhuriqi, 10)
-  FROM [stock].[dbo].[T900]  
+  FROM [stock].[dbo].[T900]
+  ORDER BY zhuriqi DESC   
    
 --exec master.dbo.xp_cmdshell 'echo 03-07                                             03-07                                                               >>C:\zd_zsone\T0002\blocknew\blocknew.cfg"';
 --exec master.dbo.xp_cmdshell 'echo 03-08                                             03-08                                                               >>C:\zd_zsone\T0002\blocknew\blocknew.cfg"';
@@ -22,7 +23,8 @@ SELECT  CONCAT('exec master.dbo.xp_cmdshell ''echo '+REPLACE(REPLACE(code,'sh.',
   zhuriqi,
    CONVERT(varchar(5), zhuriqi, 10)
   FROM [stock].[dbo].[T900] 
-  ORDER BY code desc 
+  WHERE zhuriqi>'2022-04-23'
+  ORDER BY zhuriqi desc 
 
 -- exec master.dbo.xp_cmdshell 'echo 1688508   >>C:\zd_zsone\T0002\blocknew\"03-10                                            .blk"';
 --exec master.dbo.xp_cmdshell 'echo 1600319   >>C:\zd_zsone\T0002\blocknew\"03-09                                            .blk"';
